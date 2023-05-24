bf9937c2557e535178a9b6164409760ce2346399
ss <rkdtlstjd123@naver.com>
2023-05-24 16:42:48 +0900

    리액트 네이티브 환경설정

---
+++ RNSource/App.js
@@ -0,0 +1,27 @@
+import { StatusBar } from 'expo-status-bar';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+
+      <View>
+        <Text>Another piece of text!</Text>
+      </View>
+      
+      <Text style={{margin: 16 }}>Hello World!</Text>
+
+
+      <Button title='Tap me!' />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
