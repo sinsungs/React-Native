ec15bc975de89d25b9945b1187244757a9d4d28e
ss <rkdtlstjd123@naver.com>
2023-05-25 20:41:03 +0900

    버튼 이벤트 추가

--- RNSource/App.js
+++ RNSource/App.js
@@ -1,27 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
+  const [enteredGoalText, setEnteredGoalText] = useState('');
+
+  function goalInputHandler(enteredText) {
+    setEnteredGoalText(enteredText);
+  }
+
+  function addGoalHandler() {
+    console.log(enteredGoalText);
+  }
+
   return (
-    <View style={styles.container}>
 
-      <View>
-        <Text>Another piece of text!</Text>
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.TextInput} 
+        placeholder="Your course goal"
+        onChangeText={goalInputHandler}
+        />
+        <Button title="Add Goal" onPress={addGoalHandler} />
       </View>
-      
-      <Text style={{margin: 16 }}>Hello World!</Text>
-
-
-      <Button title='Tap me!' />
+      <View style={styles.goalsContainer}>
+        <Text>List of goals......</Text>
+      </View>
     </View>
+
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  appContainer: {
+    // flex: 1, 모바일에서 확인하고 설정 해야 함 ㄴ
+    paddingTop: 50,
+    paddingHorizontal: 16
   },
+  inputContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: '#cccccc',
+  },
+  TextInput: {
+    borderWidth: 1,
+    borderColor: '#cccccc',
+    width: '70%',
+    marginRight: 8,
+    padding: 8
+  },
+  goalsContainer: {
+    flex: 4
+  }
 });
